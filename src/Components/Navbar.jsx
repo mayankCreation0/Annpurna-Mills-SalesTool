2a5ce644052952776e5ae910beff3fd878bb3ff5
Mayank-Raj24 <mayank.raj@squareyard.co.in>
2024-05-18 17:40:45 +0530

    Fix:checking auth from cookies

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -30,7 +30,6 @@ function Navbar({ mode, toggleColorMode }) {
     const [open, setOpen] = React.useState(false);
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const auth = useSelector((state) => state.auth);
 
 
 
@@ -45,149 +44,148 @@ function Navbar({ mode, toggleColorMode }) {
 
 
     return (
-        <>
-            {auth ? <div>
-                <AppBar
-                    maxWidth="100%"
-                    sx={{
-                        boxShadow: 0,
-                        // bgcolor: 'coral',
-                        backgroundImage: 'none',
-                    }}
-                >
-                    <Container maxWidth="100%">
-                        <Toolbar
-                            variant="regular"
-                            sx={(theme) => ({
+        <div>
+            <AppBar
+                maxWidth="100%"
+                sx={{
+                    boxShadow: 0,
+                    // bgcolor: 'coral',
+                    backgroundImage: 'none',
+                }}
+            >
+                <Container maxWidth="100%">
+                    <Toolbar
+                        variant="regular"
+                        sx={(theme) => ({
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            flexShrink: 0,
+                            bgcolor:
+                                theme.palette.mode === 'light'
+                                    ? 'rgba(255, 255, 255, 0.4)'
+                                    : 'rgba(0, 0, 0, 0.4)',
+                            backdropFilter: 'blur(24px)',
+                            maxHeight: 40,
+                            border: '1px solid',
+                            borderColor: 'divider',
+                            boxShadow:
+                                theme.palette.mode === 'light'
+                                    ? `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`
+                                    : '0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)',
+                        })}
+                    >
+                        <Box
+                            sx={{
+                                flexGrow: 1,
                                 display: 'flex',
                                 alignItems: 'center',
-                                justifyContent: 'space-between',
-                                flexShrink: 0,
-                                bgcolor:
-                                    theme.palette.mode === 'light'
-                                        ? 'rgba(255, 255, 255, 0.4)'
-                                        : 'rgba(0, 0, 0, 0.4)',
-                                backdropFilter: 'blur(24px)',
-                                maxHeight: 40,
-                                border: '1px solid',
-                                borderColor: 'divider',
-                                boxShadow:
-                                    theme.palette.mode === 'light'
-                                        ? `0 0 1px rgba(85, 166, 246, 0.1), 1px 1.5px 2px -1px rgba(85, 166, 246, 0.15), 4px 4px 12px -2.5px rgba(85, 166, 246, 0.15)`
-                                        : '0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)',
-                            })}
+                                ml: '-18px',
+                                px: 0,
+                            }}
                         >
-                            <Box
-                                sx={{
-                                    flexGrow: 1,
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    ml: '-18px',
-                                    px: 0,
-                                }}
-                            >
-                                <img
-                                    src={logo}
-                                    style={logoStyle}
-                                    alt="logo of sitemark"
-                                    onClick={() => navigate('/home')}
-                                />
-                                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                                    <MenuItem
-                                        onClick={() => navigate('/CustomerLists')}
-                                        sx={{ py: '6px', px: '12px' }}
-                                    >
-                                        <PeopleIcon />
-                                        <Typography variant="body2" color="text.primary">
-                                            Customer List
-                                        </Typography>
-                                    </MenuItem>
-                                    <MenuItem
-                                        onClick={() => navigate('/form')}
-                                        sx={{ py: '6px', px: '12px' }}
-                                    >
-                                        <AddCircleIcon />
-                                        <Typography variant="body2" color="text.primary">
-                                            Add Customer
-                                        </Typography>
-                                    </MenuItem>
-                                </Box>
+                            <img
+                                src={logo}
+                                style={logoStyle}
+                                alt="logo of sitemark"
+                                onClick={() => navigate('/home')}
+                            />
+                            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                                <MenuItem
+                                    onClick={() => navigate('/CustomerLists')}
+                                    sx={{ py: '6px', px: '12px' }}
+                                >
+                                    <PeopleIcon />
+                                    <Typography variant="body2" color="text.primary">
+                                        Customer List
+                                    </Typography>
+                                </MenuItem>
+                                <MenuItem
+                                    onClick={() => navigate('/form')}
+                                    sx={{ py: '6px', px: '12px' }}
+                                >
+                                    <AddCircleIcon />
+                                    <Typography variant="body2" color="text.primary">
+                                        Add Customer
+                                    </Typography>
+                                </MenuItem>
+                            </Box>
 
-                            </Box>
-                            <Box
-                                sx={{
-                                    display: { xs: 'none', md: 'flex' },
-                                    gap: 0.5,
-                                    alignItems: 'center',
-                                }}
+                        </Box>
+                        <Box
+                            sx={{
+                                display: { xs: 'none', md: 'flex' },
+                                gap: 0.5,
+                                alignItems: 'center',
+                            }}
+                        >
+                            <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
+                            <Button
+                                color="primary"
+                                variant="contained"
+                                size="small"
+                                component="a"
+                                onClick={() => handleLogout()}
+                                target="_blank"
                             >
-                                <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
-                                <Button
-                                    color="primary"
-                                    variant="contained"
-                                    size="small"
-                                    component="a"
-                                    onClick={() => handleLogout()}
-                                    target="_blank"
+                                Log out
+                            </Button>
+                        </Box>
+                        <Box sx={{ display: { sm: '', md: 'none' } }}>
+                            <Button
+                                variant="text"
+                                color="primary"
+                                aria-label="menu"
+                                onClick={toggleDrawer(true)}
+                                sx={{ minWidth: '30px', p: '4px' }}
+                            >
+                                <MenuIcon />
+                            </Button>
+                            <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
+                                <Box
+                                    sx={{
+                                        minWidth: '60dvw',
+                                        p: 2,
+                                        backgroundColor: 'background.paper',
+                                        flexGrow: 1,
+                                    }}
                                 >
-                                    Log out
-                                </Button>
-                            </Box>
-                            <Box sx={{ display: { sm: '', md: 'none' } }}>
-                                <Button
-                                    variant="text"
-                                    color="primary"
-                                    aria-label="menu"
-                                    onClick={toggleDrawer(true)}
-                                    sx={{ minWidth: '30px', p: '4px' }}
-                                >
-                                    <MenuIcon />
-                                </Button>
-                                <Drawer anchor="right" open={open} onClose={toggleDrawer(false)}>
                                     <Box
                                         sx={{
-                                            minWidth: '60dvw',
-                                            p: 2,
-                                            backgroundColor: 'background.paper',
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            alignItems: 'end',
                                             flexGrow: 1,
                                         }}
                                     >
-                                        <Box
-                                            sx={{
-                                                display: 'flex',
-                                                flexDirection: 'column',
-                                                alignItems: 'end',
-                                                flexGrow: 1,
-                                            }}
-                                        >
-                                            <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
-                                        </Box>
-                                        <MenuItem onClick={() => navigate('/CustomerLists')}>
-                                            Customer List
-                                        </MenuItem>
-                                        <MenuItem onClick={() => navigate('/form')}>
-                                            Add Customer
-                                        </MenuItem>
-                                        <Divider />
-                                        <MenuItem>
-                                            <Button
-                                                color="primary"
-                                                variant="contained"
-                                                size="small"
-                                                component="a"
-                                                onClick={() => handleLogout()}
-                                                target="_blank"
-                                            >
-                                                Log out
-                                            </Button>
-                                        </MenuItem>
+                                        <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
                                     </Box>
-                                </Drawer>
-                            </Box>
-                        </Toolbar>
-                    </Container>
-                </AppBar>
-            </div> : null}</>
+                                    <MenuItem onClick={() => navigate('/CustomerLists')}>
+                                        Customer List
+                                    </MenuItem>
+                                    <MenuItem onClick={() => navigate('/form')}>
+                                        Add Customer
+                                    </MenuItem>
+                                    <Divider />
+                                    <MenuItem>
+                                        <Button
+                                            color="primary"
+                                            variant="contained"
+                                            size="small"
+                                            component="a"
+                                            onClick={() => handleLogout()}
+                                            target="_blank"
+                                        >
+                                            Log out
+                                        </Button>
+                                    </MenuItem>
+                                </Box>
+                            </Drawer>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </div>
     );
 }
 
