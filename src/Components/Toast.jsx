b0f78e1ac5e4c07cb9a34b5e801428b8fbcea23e
Mayank-Raj24 <mayank.raj@squareyard.co.in>
2024-06-10 17:54:21 +0530

    Fix: toast issue

--- src/Components/Toast.jsx
+++ src/Components/Toast.jsx
@@ -1,7 +1,7 @@
-// Toast.js
 import React, { useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import '../Styles/toast.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { hideToast } from '../Store/Reducers/Reducer';
 
