8cf7d4f91d359383502c6df422015a1fab10084f
Mayank-Raj24 <mayank.raj@squareyard.co.in>
2024-06-13 13:27:30 +0530

    Fix: interest calculation issue and api calling

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ if(sessionStorage.getItem('mode') === null){
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <Toast />
@@ -27,7 +26,6 @@ root.render(
         <Analytics />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
